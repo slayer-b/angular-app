09d568b9539f6d52c3a3ef6888a05779ca297874
Dmitriy Demchuk <slayer@Dmitriys-MacBook-Pro.local>
2014-06-08 14:20:06 +0300

    added columns with differend timing

--- public/javascripts/compile.js
+++ public/javascripts/compile.js
@@ -12,20 +12,40 @@ angular.module("my-compile", [])
                 compiled($scope);
             }
         };
-})
-    .controller("MyCtrl", function($scope, $timeout, MyService, $interval) {
-        $scope.msg = "Hi, you";
-        var count = 0;
+}).controller("SlidersCtrl", function($scope, $timeout, $interval, MyService) {
+        $scope.slide = false;
+        var id = "#sliders";
+        var html = "<div class=\"element\" ng-include=\"'/assets/html/sliders-partial.html'\"></div>";
+        var i = 0;
+        var next = 1;
         $interval(function() {
-            count += 1;
-            $scope.msg = count;
+            i = i + 1;
+            $scope.slide = !$scope.slide;
+            if (i == next) {
+                var newScope = $scope.$new();
+                newScope.i = 0;
+                newScope.number = next;
+                $interval(function() {
+                    newScope.i += 1;
+                }, 1000);
+                i = 0;
+                next = 2 * next;
+                MyService.compile(html, id, newScope);
+            }
         }, 1000);
-        $timeout(function() {
-            $("#my-div").html("Removed");
-        }, 5000);
-        var html = "<div>{{msg}}</div>";
-        MyService.compile(html, "#my-div", $scope);
+}).controller("MyCtrl", function($scope, $timeout, MyService, $interval) {
+    $scope.msg = "Hi, you";
+    var count = 0;
+    $interval(function() {
+        count += 1;
+        $scope.msg = count;
+    }, 1000);
+    $timeout(function() {
+        $("#my-div").html("Removed");
+    }, 5000);
+    var html = "<div>{{msg}}</div>";
+    MyService.compile(html, "#my-div", $scope);
 
-        var html2 = "<div ng-include=\"'/assets/html/partial.html'\"></div>";
-        MyService.compile(html2,"#my-div", $scope);
-    });
+    var html2 = "<div ng-include=\"'/assets/html/partial.html'\"></div>";
+    MyService.compile(html2,"#my-div", $scope);
+});
